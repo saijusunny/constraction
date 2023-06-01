b75f9f70c55a9afb396b8d43a65bc4a88ce518e5
Saiju Sunny <saijusunny1301@gmail.com>
2023-06-01 19:24:17 +0530

    con

---
+++ app1/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render,redirect
+
+# Create your views here.
+def login(request):
+    return render(request,'login.html')
+def about(request):
+    return render(request,'about.html')
+def contact(request):
+    return render(request,'contact.html')
+
+
+
